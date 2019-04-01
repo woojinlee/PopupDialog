df4c18ab7c1e758d77a113691a2aa4a65bda70a8
Amon <amon@cupist.com>
2019-04-01 15:08:13 +0900

    change fade in duration

--- PopupDialog/Classes/TransitionAnimations.swift
+++ PopupDialog/Classes/TransitionAnimations.swift
@@ -54,7 +54,7 @@ final internal class BounceUpTransition: TransitionAnimator {
         switch direction {
         case .in:
             to.view.bounds.origin = CGPoint(x: 0, y: -from.view.bounds.size.height)
-            UIView.animate(withDuration: 0.6, delay: 0.0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0, options: [.curveEaseOut], animations: { [weak self] in
+            UIView.animate(withDuration: inDuration, delay: 0.0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0, options: [.curveEaseOut], animations: { [weak self] in
                 guard let self = self else { return }
                 self.to.view.bounds = self.from.view.bounds
             }, completion: { _ in
