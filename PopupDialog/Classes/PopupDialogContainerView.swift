f408b0d109bf3963b2b3f9da6203a1e32ff0c18b
Amon <amon@cupist.com>
2019-04-01 15:07:09 +0900

    apply preparedWidth without condition

--- PopupDialog/Classes/PopupDialogContainerView.swift
+++ PopupDialog/Classes/PopupDialogContainerView.swift
@@ -170,12 +170,8 @@ final public class PopupDialogContainerView: UIView {
         var constraints = [NSLayoutConstraint]()
 
         // Shadow container constraints
-        if UIDevice.current.userInterfaceIdiom == UIUserInterfaceIdiom.pad {
-            let metrics = ["preferredWidth": preferredWidth]
-            constraints += NSLayoutConstraint.constraints(withVisualFormat: "H:|-(>=40)-[shadowContainer(==preferredWidth@900)]-(>=40)-|", options: [], metrics: metrics, views: views)
-        } else {
-            constraints += NSLayoutConstraint.constraints(withVisualFormat: "H:|-(>=10,==20@900)-[shadowContainer(<=340,>=300)]-(>=10,==20@900)-|", options: [], metrics: nil, views: views)
-        }
+        let metrics = ["preferredWidth": preferredWidth]
+        constraints += NSLayoutConstraint.constraints(withVisualFormat: "H:|-(>=40)-[shadowContainer(==preferredWidth@900)]-(>=40)-|", options: [], metrics: metrics, views: views)
         constraints += [NSLayoutConstraint(item: shadowContainer, attribute: .centerX, relatedBy: .equal, toItem: self, attribute: .centerX, multiplier: 1, constant: 0)]
         centerYConstraint = NSLayoutConstraint(item: shadowContainer, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0)
         
